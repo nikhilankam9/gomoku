08ccdbefac4ec1a29cbd33f23e567b86e3fd1ad2
nikhilankam9 <nikhilgdk@gmail.com>
2021-11-29 23:01:15 -0600

    Test cases

--- unit_test.cpp
+++ unit_test.cpp
@@ -71,10 +71,25 @@ void Test_checkVertical() {
     cout<<endl;
 }
 
+void Test_checkDiagonal_1() {
+    cout<<"Test_checkDiagonal_1\n";
+    test_board[0][0] = test_board[1][1] = test_board[2][2] = test_board[3][3] = test_board[4][4] = 1;
+    assert(checkDiagonal_1(test_board, 1, 0, 0) == true);
+    test_board[0][0] = test_board[1][1] = test_board[2][2] = test_board[3][3] = test_board[4][4] = 0;
+    cout<<"Case1: Passed!\n";
+
+    test_board[0][0] = test_board[1][1] = test_board[3][3] = test_board[4][4] = test_board[5][5] = \
+    test_board[6][6] = test_board[7][7] = 1;
+    assert(checkDiagonal_1(test_board, 1, 0, 0) == true);
+    test_board[0][0] = test_board[1][1] = test_board[3][3] = test_board[4][4] = test_board[5][5] = \
+    test_board[6][6] = test_board[7][7] = 0;
+    cout<<"Case2: Passed!\n";
+}
 
 int main() {
     cout<<"Tests:\n";
     Test_checkHorizontal();
     Test_checkVertical();
+    Test_checkDiagonal_1();
     return 0;
 }
